6921b5156bfee29d37705c75f73a47bd166a98d3
Luqman Farhan <luqmn.frhn@gmail.com>
2026-05-08 00:32:47 +0800

    feat: add @astrojs/sitemap integration and update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+  site: 'https://medivironclinic.com',
+  integrations: [sitemap()],
 });
